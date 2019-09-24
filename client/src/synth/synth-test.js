2a877803585044bd3c7c20c48329f4f8ee291739
Alex <ajbarbati@gmail.com>
2019-09-24 19:58:29 -0400

    added return

--- client/src/synth/synth-test.js
+++ client/src/synth/synth-test.js
@@ -2,7 +2,7 @@ import React from "react"
 
 
 function Synth () {
-// this creates the audio context
+    // this creates the audio context
 const context = new (window.AudioContext || window.webkitAudioContext) ()
 
 // creates the repeating ocillator 
@@ -11,9 +11,13 @@ const context = new (window.AudioContext || window.webkitAudioContext) ()
 const oscillator = context.createOscillator()
 
 oscillator.type = 'sine'
-oscillator.frequency.value = 440
+oscillator.frequency.value = 200
 oscillator.connect(context.destination)
 oscillator.start() 
+
+    return (
+        <div>farts</div>
+    )
 
 }
 
