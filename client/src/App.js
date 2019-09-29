98ec2069f473e827deabe982b88af71187eba875
sinid <49916315+oinksu@users.noreply.github.com>
2019-09-29 16:46:26 -0400

    setting up placeholder folder for componets and their styles. Moved Synth componets inside components folder. Added Sidebar componet folder, placeholder styling till I figure out some things

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Me from './components/Me'
-import Synth from './synth/synth-test'
+import Synth from './components/Synth'
 import withAuth from './components/withAuth'
+import Header from './components/Header'
+
 
 function App () {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/me" component={withAuth(Me)} />
-          <Route path="/" component={Synth} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/me' component={withAuth(Me)} />
+          <Route path='/' component={Synth} />
         </Switch>
       </div>
     </Router>
