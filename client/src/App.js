17e0fbc209bbf312f295c5eb3e5a9188eefbcbbe
Dante Fragapane <dante.fragapane@gmail.com>
2019-09-28 21:14:36 -0400

    Got logout mostly working

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,57 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Nav from './components/Nav'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Me from './components/Me'
 import Synth from './synth/synth-test'
 import withAuth from './components/withAuth'
 
-function App () {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/me" component={withAuth(Me)} />
-          <Route path="/" component={Synth} />
-        </Switch>
-      </div>
-    </Router>
-  )
+class App extends React.Component {
+  state = {
+    username: '',
+    loggedIn: false
+  }
+
+  updateUser = function () {
+    fetch('/api/getCurrentUser')
+      .then((res) => {
+        if (res.status === 200) return res.json()
+        return { username: null }
+      })
+      .then((json) => {
+        if (json.username) {
+          return this.setState({ username: json.username, loggedIn: true })
+        }
+        return
+      })
+  }
+
+  componentDidMount () {
+    this.updateUser()
+  }
+
+  // componentDidUpdate () {
+  //   this.updateUser()
+  // }
+
+  render () {
+    return (
+      <Router>
+        <Nav username={this.state.username} />
+        <div>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/profile/:user" component={withAuth(Me)} />
+            <Route path="/me" component={withAuth(Me)} />
+            <Route path="/" component={Synth} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
---
+++ client/src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function logout () {
+  fetch('/logout', { method: 'POST', credentials: 'same-origin' })
+}
+
+export default class Nav extends React.Component {
+  render () {
+    if (this.props.username) {
+      return (
+        <div>
+          <p>Hello {this.props.username}</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )
+    }
+
+    return (
+      <p>
+        Hello! <a href="/login">Login?</a>
+      </p>
+    )
+  }
+}
