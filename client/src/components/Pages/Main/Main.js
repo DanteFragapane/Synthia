6c702d3653caea2cdd1da53d394c6f82a7e4ae90
sinid <49916315+oinksu@users.noreply.github.com>
2019-10-07 15:05:26 -0400

    hidden sidebar with mock items implemented by next commit fit layout and mock keayboar

--- client/src/components/Pages/Main/Main.js
+++ client/src/components/Pages/Main/Main.js
@@ -6,21 +6,56 @@ import Footer from '../../Footer'
 import SynthApp from '../../Synth/index'
 import Sidebar from '../../Sidebar'
 import Table from '../../Table'
-import '../../HorizontalBar'
 import HorizontalBar from '../../HorizontalBar'
+import Toolbar from '../../Toolbar'
+import SideDrawer from '../../SideDrawer/SideDrawer'
+import Backdrop from '../../Backdrop/Backdrop'
 
-function Main () {
-  return (
-    <div className='wrapper'>
-      <div className='one'><Header /></div>
-      <div className='two'><Sidebar /></div>
-      <div className='three'><SynthApp /></div>
-      <div className='four'><Table /></div>
-      <div className='five'><Table /></div>
-      <div className='six'><Table /></div>
-      <div className='seven'><HorizontalBar /></div>
-      <div className='eight'><Footer /></div>
-    </div>
-  )
+
+
+class Main extends React.Component {
+
+  state = {
+    sideDrawerOpen: false
+  }
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => { 
+     return {sideDrawerOpen: !prevState.sideDrawerOpen }
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false})
+  }
+
+render() {
+
+let sideDrawer = null
+let backdrop = null
+
+if (this.state.sideDrawerOpen) {
+
+  backdrop = <Backdrop click={this.backdropClickHandler} />
 }
+
+return (
+  <div style={{ height: '100%' }} className='wrapper'>
+    <div className='one'><Header /></div>
+    <div className='two'><Toolbar drawerClickHandler={this.drawerToggleClickHandler} /></div>
+    <SideDrawer show={this.state.sideDrawerOpen}/>
+    {backdrop}
+    <div className='five'><SynthApp /></div>
+    <div className='six'><Table /></div>
+    <div className='seven'><Table /></div>
+    <div className='nine'><HorizontalBar /></div>
+    <div className='ten'><Footer /></div>
+  </div>
+)
+
+}
+
+}
+
+
 export default Main
--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -1,3 +1,25 @@
 import React from 'react'
 
-const toolbar = props => ();
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
+import './Toolbar.css'
+
+const toolbar = props => (
+  <header className='toolbar'>
+    <nav className='toolbar__navigation'>
+      <div>
+        <DrawerToggleButton click={props.drawerClickHandler} />
+      </div>
+      <div className='toolbar__logo'><a href='/'>THE LOGO </a></div>
+      <div className='spacer' />
+      <div className='toolbar_navigation-items'>
+        <ul>
+          <li><a href='/'>Products</a></li>
+          <li><a href='/'>Products</a></li>
+        </ul>
+      </div>
+
+    </nav>
+  </header>
+)
+
+export default toolbar
