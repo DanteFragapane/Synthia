bd4e374d4d646d89c97c9dc5ca5e083d05cde8f8
sinid <49916315+oinksu@users.noreply.github.com>
2019-10-15 14:12:05 -0400

    configuring keyboard keys

--- client/src/components/Synth/Synth.js
+++ client/src/components/Synth/Synth.js
@@ -10,16 +10,16 @@ export default class Synthesizer extends React.Component {
     super(props)
     this.keys = [
       { name: 'C', freq: 261.63 },
-      { name: 'C#', freq: 277.18 },
+      { name: 'CSH', freq: 277.18 },
       { name: 'D', freq: 293.66 },
-      { name: 'D#', freq: 311.13 },
+      { name: 'DSH', freq: 311.13 },
       { name: 'E', freq: 329.63 },
       { name: 'F', freq: 349.23 },
-      { name: 'F#', freq: 369.99 },
+      { name: 'FSH', freq: 369.99 },
       { name: 'G', freq: 392.0 },
-      { name: 'G#', freq: 415.3 },
+      { name: 'GSH', freq: 415.3 },
       { name: 'A', freq: 440.0 },
-      { name: 'A#', freq: 466.16 },
+      { name: 'ASH', freq: 466.16 },
       { name: 'B', freq: 493.88 },
       { name: 'C1', freq: 523.25 }
     ]
